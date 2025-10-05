f3a73f16a161dc35a8d6ac4652835d457c077837
zyx <3051857358@qq.com>
2025-10-05 12:09:47 +0800

    feature:开发短连接后管系统通用持久层实体对象属性填充

---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/config/MyMetaObjectHandler.java
@@ -0,0 +1,25 @@
+package com.nageoffer.shortlink.admin.config;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+//通过mybatisplus提供的接口实现自动填充
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        // TODO 这里使用LocalDateTime会导致mybatisplus匹配字段类型的时候失效
+        this.strictInsertFill(metaObject, "createTime", Date::new, Date.class); //参数分别为源对象，字段名，获取值的方法，字段类型
+        this.strictInsertFill(metaObject, "updateTime", Date::new, Date.class); //参数分别为源对象，字段名，获取值的方法，字段类型
+        this.strictInsertFill(metaObject, "delFlag", () ->0, Integer.class); //参数分别为源对象，字段名，获取值的方法，字段类型
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.strictInsertFill(metaObject, "createTime", LocalDateTime::now, LocalDateTime.class); //参数分别为源对象，字段名，获取值的方法，字段类型
+    }
+}
