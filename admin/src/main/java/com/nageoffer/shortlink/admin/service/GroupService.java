28178e1117b978b7c12d80960790cf8bc0c24c8e
zyx <3051857358@qq.com>
2025-10-06 17:43:02 +0800

    feature:初始化短链接分组

--- admin/src/main/java/com/nageoffer/shortlink/admin/service/GroupService.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/service/GroupService.java
@@ -2,9 +2,11 @@ package com.nageoffer.shortlink.admin.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.nageoffer.shortlink.admin.dao.entity.GroupDO;
+import org.springframework.stereotype.Service;
 
 /**
  * 短连接分组服务接口
  */
+@Service
 public interface GroupService extends IService<GroupDO> {
 }
