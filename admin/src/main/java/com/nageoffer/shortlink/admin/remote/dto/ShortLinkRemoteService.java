0576e083d7b837d9d5057cd385405bd87257d10d
zyx <3051857358@qq.com>
2025-10-13 21:05:12 +0800

    feature: 短连接后管系统调用中台请求

---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/remote/dto/ShortLinkRemoteService.java
@@ -0,0 +1,49 @@
+package com.nageoffer.shortlink.admin.remote.dto;
+
+import cn.hutool.http.HttpUtil;
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.TypeReference;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.nageoffer.shortlink.admin.common.convention.result.Result;
+import com.nageoffer.shortlink.admin.remote.dto.req.ShortLinkCreateReqDTO;
+import com.nageoffer.shortlink.admin.remote.dto.req.ShortLinkPageReqDTO;
+import com.nageoffer.shortlink.admin.remote.dto.resp.ShortLinkCreateRespDTO;
+import com.nageoffer.shortlink.admin.remote.dto.resp.ShortLinkPageRespDTO;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 短连接中台远程调用服务
+ */
+public interface ShortLinkRemoteService {
+
+    /**
+     * 分页查询短连接
+     * @param requestParam
+     * @return
+     */
+    default Result<IPage<ShortLinkPageRespDTO>> pageShortLink(ShortLinkPageReqDTO requestParam) {
+        Map<String ,Object> requestMap = new HashMap<>();
+        requestMap.put("gid",requestParam.getGid());
+        requestMap.put("current",requestParam.getCurrent());
+        requestMap.put("size",requestParam.getSize());
+        String resultPageStr = HttpUtil.get("http://127.0.0.1:8001/api/shot-link/v1/page", requestMap);
+
+        return JSON.parseObject(resultPageStr, new TypeReference<>() {
+        });
+    }
+
+
+    /**
+     * 创建短链接
+     * @param requestParam
+     * @return
+     */
+    default Result<ShortLinkCreateRespDTO> createShortLink(ShortLinkCreateReqDTO requestParam){
+        String resultBodyStr = HttpUtil.post("http://127.0.0.1:8001/api/shot-link/v1/create", JSON.toJSONString(requestParam));
+        //通过TypeReference告诉fastjson反序列化时的DTO中泛型成员变量的具体类型
+        return JSON.parseObject(resultBodyStr, new TypeReference<Result<ShortLinkCreateRespDTO>>() {
+        });
+    }
+}
