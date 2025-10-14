d1219758e44c8e244d85c79e57dede566e4902ea
zyx <3051857358@qq.com>
2025-10-14 21:09:15 +0800

    opetimize:优化短连接分组排名

--- admin/src/main/java/com/nageoffer/shortlink/admin/dto/resp/ShortLinkGroupRespDTO.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/dto/resp/ShortLinkGroupRespDTO.java
@@ -18,11 +18,6 @@ public class ShortLinkGroupRespDTO{
     private String name;
 
     /**
-     * 创建该分组的用户名
-     */
-    private String username;
-
-    /**
      * 分组排序
      */
     private Integer sortOrder;
