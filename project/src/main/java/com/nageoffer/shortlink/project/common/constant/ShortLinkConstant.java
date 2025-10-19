bbd9b177378a751b787c3c976f50339007a6ad28
zyx <3051857358@qq.com>
2025-10-19 16:08:15 +0800

    optimize: 开发短连接创建缓存预热

---
+++ project/src/main/java/com/nageoffer/shortlink/project/common/constant/ShortLinkConstant.java
@@ -0,0 +1,11 @@
+package com.nageoffer.shortlink.project.common.constant;
+
+/**
+ * 短链接常量
+ */
+public class ShortLinkConstant {
+    /**
+     * 短连接默认有效时间 30 天，单位：毫秒
+     */
+    public static  final long DEFAULT_CACHE_VALID_TIME = 2526560000L;
+}
