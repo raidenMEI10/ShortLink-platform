21c0b81b22e2141f872811fea862da05566458af
zyx <3051857358@qq.com>
2025-11-14 23:50:55 +0800

    refactor: 重构短链接系统网关层

--- admin/src/main/java/com/nageoffer/shortlink/admin/common/enums/UserErrorCodeEnum.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/common/enums/UserErrorCodeEnum.java
@@ -25,7 +25,7 @@ import com.nageoffer.shortlink.admin.common.convention.errorcode.IErrorCode;
  */
 public enum UserErrorCodeEnum implements IErrorCode {
 
-    USER_TOKEN_FAIL("A000200", "用户Token验证失败"),
+//    USER_TOKEN_FAIL("A000200", "用户Token验证失败"),
 
     USER_NULL("B000200", "用户记录不存在"),
 
